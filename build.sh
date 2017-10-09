40a984d685ec95a0e36f337c6f69f93e6c10983a
Frederick Robinson <frederick.robinson@gmail.com>
2017-10-08 18:33:08 -0700

    first commit

---
+++ build.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+CURLFLAGS="--compressed"
+CHAPTERS=33
+OUTPREFIX="tgs"
+OUTNAME="$OUTPREFIX-$CHAPTERS"
+TMP1="/tmp/tmp1"
+
+echo $CHAPTERS > numchapters
+
+# create source dir if it doesn't exit
+if [[ ! -e source ]]; then
+    mkdir source
+fi
+
+# check for undownloaded chapters
+for x in `seq $CHAPTERS`
+do
+    filename="source/$x.html"
+    url="http://gravitytales.com/novel/the-good-student/tgs-chapter-$x"
+    if [[ ! -e $filename ]]; then
+        echo "Downloading Chapter $x"
+        curl $CURLFLAGS -o $filename $url
+    fi
+done
+
+# create markdown dir if it doesn't exit
+if [[ ! -e markdown ]]; then
+    mkdir markdown
+fi
+
+# reformat to markdown
+for x in `seq $CHAPTERS`
+do
+    input="source/$x.html"
+    output="markdown/$x.md"
+    if [[ ! -e $output ]]; then
+        echo "stripping headers/footers: Chapter $x"
+        grep -A 1 "chapterContent" $input | tail -n 1 > $TMP1
+        echo "converting to markdown: Chapter $x"
+        pandoc $TMP1 -f html -t markdown -o $output
+    fi
+done
+
+# create cleanmd dir if it doesn't exit
+if [[ ! -e cleanmd ]]; then
+    mkdir cleanmd
+fi
+
+# remove div tags and rewrite chapter tag
+for x in `seq $CHAPTERS`
+do
+    padded=`printf '%03d' $x`
+    input="markdown/$x.md"
+    output="cleanmd/$padded.md"
+    if [[ ! -e $output ]]; then
+        echo "cleaning markdown: Chapter $x"
+
+        #chapter titles
+        sed -i 's;<span style="font-size: 18px">\(CHAPTER [^<]*\)</span>;# \1;' $input 
+
+		sed -i 's;\\;;' $input
+
+        #breaks
+        sed -i 's/- break -/-----/' $input
+
+        # reformat
+        pandoc $input -o $TMP1
+        mv $TMP1 $output
+    fi
+done
+
+# create cover dir if it doesn't exit
+if [[ ! -e cover ]]; then
+    mkdir cover
+fi
+if [[ ! -e cover/raw_cover.jpg ]]; then
+    echo "Fetching cover image"
+    curl -o cover/raw_cover.jpg https://cdn.gravitytales.com/images/covers/the-good-student.jpg
+fi
+if [[ ! -e cover/cover.jpg ]]; then
+    echo "Resizing cover image"
+    convert cover/raw_cover.jpg -resize 600x900\! cover/cover.jpg
+fi
+
+# Produce epub if current doesn't exist
+if [[ ! -e ${OUTNAME}.epub ]]; then
+    # ask to remove old ones
+    rm -i $OUTPREFIX-*.epub
+    echo 'building epub...'
+    pandoc -S -o ${OUTNAME}.epub title.txt `ls cleanmd/*md | sort -n` --toc --epub-cover-image=cover/cover.jpg
+fi
+
+if [[ ! -e ${OUTNAME}.mobi ]]; then
+    rm -i $OUTPREFIX-*.mobi
+    echo 'building mobi...'
+    kindlegen ${OUTNAME}.epub
+
+    rm -i mobi7-$OUTPREFIX-*.mobi
+    echo 'extracting mobi7...'
+    # cd .. && git clone https://github.com/kevinhendricks/KindleUnpack.git
+    python ../KindleUnpack/lib/kindleunpack.py -s ${OUTNAME}.mobi
+    cp ${OUTNAME}/mobi7-${OUTNAME}.mobi .
+    rm -rf $OUTNAME
+fi
+
